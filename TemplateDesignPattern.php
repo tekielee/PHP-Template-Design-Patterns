a801a4c6f9f1706517ad5a5e0ba6c7a359d752e6
Cuong Le <tekielee@yahoo.com>
2021-11-11 12:28:10 -0800

    initial commit

---
+++ TemplateDesignPattern.php
@@ -0,0 +1,67 @@
+<?php
+class Game {
+	function __constructor() {}
+	
+	public function initialize(): void {}
+	
+	public function startPlay(): void {}
+	
+	public function endPlay(): void {}
+	
+	public function play(): void {
+		$this->initialize();
+		$this->startPlay();
+		$this->endPlay();
+	}
+}
+
+class Cricket extends Game {
+	function __constructor() {
+		parent::__constructor();
+	}
+	
+	public function endPlay(): void {
+		echo 'Cricket Game Finished!<br/>';
+	}
+	
+	public function initialize(): void {
+		echo 'Cricket Game Initialized!<br/>';
+	}
+	
+	public function startPlay(): void {
+		echo 'Cricket Game Started. Enjoy the game!<br/>';
+	}
+	
+	public function play(): void {
+		parent::play();
+	}
+}
+
+class Football extends Game {
+	function __constructor(): void {
+		parent::__constructor();
+	}
+	
+	public function endPlay(): void {
+		echo 'Football Game Finished!<br/>';
+	}
+	
+	public function initialize(): void {
+		echo 'Football Game Initialized!<br/>';
+	}
+	
+	public function startPlay(): void {
+		echo 'Football Game Started. Enjoy the game!<br/>';
+	}
+	
+	public function play(): void {
+		parent::play();
+	}
+}
+
+$cricket_game = new Cricket();
+$cricket_game->play();
+echo '<br/>';
+$football_game = new Football();
+$football_game->play();
+?>
